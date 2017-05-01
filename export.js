cd37d4702251bacdbec3fe3b88aefe11b77e173d
Anders Skarby <anders@MacBook-Pro.local>
2017-05-01 13:53:25 +0200

    Now able to import instascan using a module importer (from node_modules) without depending upon shims. Dists are still build with these bundled

--- export.js
+++ export.js
@@ -1,1 +1,4 @@
+require('babel-polyfill');
+require('webrtc-adapter');
+
 window.Instascan = require('./index');
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-require('babel-polyfill');
-require('webrtc-adapter');
-
 var Instascan = {
   Scanner: require('./src/scanner'),
   Camera: require('./src/camera')
