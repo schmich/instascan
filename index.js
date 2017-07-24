4fb576687766473728cb7d213569a0ca42d6ad87
Paco <pakokrew@gmail.com>
2017-07-24 19:08:35 +0200

    Edit build to correct webpack imports

--- index.js
+++
@@ -1,9 +0,0 @@
-require('babel-polyfill');
-require('webrtc-adapter');
-
-var Instascan = {
-  Scanner: require('./src/scanner'),
-  Camera: require('./src/camera')
-};
-
-module.exports = Instascan;
