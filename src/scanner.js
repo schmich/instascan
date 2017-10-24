04b531ada6be3c3f261f00722bbea4c9996543cd
sidharth <sidharth.beis.11@acharya.ac.in>
2017-10-24 09:40:32 +0530

    Added tweak for 2d data matrix scan

--- src/scanner.js
+++ src/scanner.js
@@ -142,7 +142,7 @@ class Analyzer {
       ZXing.HEAPU8[this.imageBuffer + j] = Math.trunc((r + g + b) / 3);
     }
 
-    let err = ZXing._decode_qr(this.decodeCallback);
+    let err = ZXing._decode_any(this.decodeCallback);
     if (err) {
       return null;
     }
